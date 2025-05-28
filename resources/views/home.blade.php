97bd93424989d0f810872eec9038a350ed2d5391
all <alhaidarfataarizqyfauzi@gmail.com>
2025-05-28 08:58:48 +0700

    database

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <div class="container">
-        <h1>lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</h1>
+        <h1>Al haidar Fataa</h1>
     </div>
 </body>
 </html>
